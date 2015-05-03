08b0bd665b14823f21732e898b237c66455dc5b9
mjszymko <mszymkowskicca2013@gmail.com>
2015-05-03 15:29:54 -0400

    loadgame

--- ScrabbleGame/src/code/Driver.java
+++ ScrabbleGame/src/code/Driver.java
@@ -5,11 +5,14 @@ import java.io.IOException;
 public class Driver {
 	
 	public static void main(String args[]) throws IOException{
+		/*
 		Scrabble s = new Scrabble(2);
 		GUI g = new GUI(s);
 		g.update();
 		Dialog d = new Dialog(s);
-		SaveGame sg = new SaveGame(s);
+		*/
+		//SaveGame sg = new SaveGame(s);
+		LoadGame lg = new LoadGame();
 	}
 	
 	
--- ScrabbleGame/src/code/LoadGame.java
+++ ScrabbleGame/src/code/LoadGame.java
@@ -1,5 +1,39 @@
 package code;
 
-public class LoadGame {
+import java.awt.FileDialog;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+import javax.swing.JFrame;
+
+public class LoadGame {
+	
+	private String _fileToRead;
+	
+	public LoadGame() throws IOException{
+
+	JFrame jf = new JFrame();
+	FileDialog chooser = new FileDialog(jf,"Save your file",FileDialog.LOAD);
+	chooser.setDirectory("C:\\"); chooser.setFile("*.txt");
+	chooser.setVisible(true); String filename = chooser.getFile();
+	String path = chooser.getDirectory(); System.out.println(path);
+	String filetoRead = path + filename;
+	_fileToRead = filetoRead;
+	File file = new File(_fileToRead);
+	FileReader fw = new FileReader(file.getAbsoluteFile());
+	BufferedReader bw = new BufferedReader(fw);
+    String s = (bw.readLine());
+    String delims = "%";
+    String[] tokens = s.split(delims);
+    
+    for (int i = 0; i < tokens.length; i++){
+        System.out.println(tokens[i]);
+    }
+    
+	}
 }
+
+
